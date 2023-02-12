26ba96b5d7b9299bf55bd434f383693fc58e8ff2
lcassis10 <lcassis10@gmail.com>
2023-02-12 23:00:50 +0000

    Create get_quantity_data function to get data string from the user

--- run.py
+++ run.py
@@ -12,8 +12,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('sales_spreadsheet')
 
-sales = SHEET.worksheet("price")
+def get_quantity_data():
+    """
+    Get quantity data from the user.
+    """
+    print("Please enter the quantity of sales for each item")
+    print("Data should be only numbers\n")
 
-data = sales.get_all_values()
+    data_str1 = input("Number of Guinness sold: ")
+    data_str2 = input("Number of Fish and Chips sold: ")
+    data_str3 = input("Number of Brownies sold: ")
 
-print(data)
+    print(f"The number of Guinness sold is {data_str1}. The number of Fish and Chips sold is {data_str2}. The number of Brownies sold is {data_str3} ")
+
+
+get_quantity_data()
